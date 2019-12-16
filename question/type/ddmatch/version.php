633db5209be153313e39eab5492606a11d59db1d
Nick Phillips <nick.phillips@otago.ac.nz>
2019-12-16 14:18:58 +1300

    Udate qtype_ddmatch to 2019111900.

--- question/type/ddmatch/version.php
+++ question/type/ddmatch/version.php
@@ -23,11 +23,11 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'qtype_ddmatch';
-$plugin->version   = 2019040900;
+$plugin->version   = 2019111900;
 
 $plugin->requires  = 2013050100;
 $plugin->dependencies = array(
     'qtype_match' => 2013050100,
 );
-$plugin->release = '1.6 Drag and drop matching question for Moodle 2.6, 2.7, 2.8, 2.9, 3.0, 3.1, 3.2, 3.3, 3.4';
+$plugin->release = '1.6 Drag and drop matching question for Moodle 2.6, 2.7, 2.8, 2.9, 3.0, 3.1, 3.2, 3.3, 3.4, 3.6, 3.7, 3.8';
 $plugin->maturity  = MATURITY_STABLE;
