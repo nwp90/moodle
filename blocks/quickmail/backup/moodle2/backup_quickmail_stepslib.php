69e0861e9121df1cda3eeda8b8bcfbaf993c8f3d
Nick Phillips <nick.phillips@otago.ac.nz>
2019-12-19 16:23:35 +1300

    Update block_quickmail to v2

--- blocks/quickmail/backup/moodle2/backup_quickmail_stepslib.php
+++ blocks/quickmail/backup/moodle2/backup_quickmail_stepslib.php
@@ -16,70 +16,66 @@
 
 /**
  * @package    block_quickmail
- * @copyright  2008-2017 Louisiana State University
- * @copyright  2008-2017 Adam Zapletal, Chad Mazilly, Philip Cali, Robert Russo
+ * @copyright  2008 onwards Louisiana State University
+ * @copyright  2008 onwards Chad Mazilly, Robert Russo, Jason Peak, Dave Elliott, Adam Zapletal, Philip Cali
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
+
+defined('MOODLE_INTERNAL') || die();
 
 class backup_quickmail_block_structure_step extends backup_block_structure_step {
     protected function define_structure() {
         global $DB;
 
-        $params = array('courseid' => $this->get_courseid());
-        $context = context_course::instance( $params['courseid']);
-        //LOGS
-        $quickmail_logs = $DB->get_records('block_quickmail_log', $params);
-        $include_history = $this->get_setting_value('include_quickmail_log');
+        $params = array('course_id' => $this->get_courseid());
+        $context = context_course::instance( $params['course_id']);
+        // Logs for backups.
+        $quickmaillogs = $DB->get_records('block_quickmail_messages', $params);
+        $includehistory = $this->get_setting_value('include_quickmail_log');
 
-        // QM BLOCK CONFIG BACKUP
-        // attempt to create block settings step for quickmail, so people can restore their quickmail settings 
+        // Quickmail backup config.
+        // Attempt to create block settings step for quickmail, so people can restore their quickmail settings.
+        $paramstwo = array('coursesid' => $this->get_courseid());
+        $quickmailblocklevelsettings = $DB->get_records('block_quickmail_config', $paramstwo);
+        $includeconfig = $this->get_setting_value('include_quickmail_config');
 
-        // WHY IS CONFIGS TABLE SET TO COURSES WITH AN S ID????????
-        $paramsTwo = array('coursesid' => $this->get_courseid());
-        $quickmail_block_level_settings = $DB->get_records('block_quickmail_config', $paramsTwo);
-        $include_config = $this->get_setting_value('include_quickmail_config');
-
-        //LOGS
-        $backup_logs_and_settings = new backup_nested_element('emaillogs', array('courseid'), null);
+        // Backup logs and settings.
+        $backuplogsandsettings = new backup_nested_element('emaillogs', array('course_id'), null);
 
         $log = new backup_nested_element('log', array('id'), array(
-            'userid', 'courseid', 'alternateid', 'mailto', 'subject',
-            'message', 'attachment', 'format', 'time','failuserids','additional_emails'
+            'course_id', 'user_id', 'message_type', 'notification_id', 'alternate_email_id',
+            'signature_id', 'subject', 'body', 'editor_format', 'sent_at', 'to_send_at',
+            'is_draft', 'send_reciept', 'send_to_mentors', 'is_sending', 'no_reply',
+            'usermodified', 'timecreated', 'timemodified', 'timedeleted'
         ));
 
-        // courseid name value
-        $quickmail_settings = new backup_nested_element('block_level_setting', array('id'), array(
-            'courseid', 'name', 'value'
+        // Courseid name value.
+        $quickmailsettings = new backup_nested_element('block_level_setting', array('id'), array(
+            'coursesid', 'name', 'value'
         ));
 
+        $backuplogsandsettings->add_child($log);
 
-        $backup_logs_and_settings->add_child($log);
+        $backuplogsandsettings->add_child($quickmailsettings);
 
-        $backup_logs_and_settings->add_child($quickmail_settings);
+        $backuplogsandsettings->set_source_array(array((object)$params));
 
-        $backup_logs_and_settings->set_source_array(array((object)$params));
-
-        if (!empty($quickmail_logs) and $include_history) {
+        if (!empty($quickmaillogs) and $includehistory) {
             $log->set_source_sql(
-                'SELECT * FROM {block_quickmail_log}
-                WHERE courseid = ?', array(array('sqlparam' => $this->get_courseid()))
+                'SELECT * FROM {block_quickmail_messages}
+                WHERE course_id = ?', array(array('sqlparam' => $this->get_courseid()))
             );
         }
 
-        if (!empty($quickmail_block_level_settings) and $include_config) {
-            $quickmail_settings->set_source_sql(
+        if (!empty($quickmailblocklevelsettings) and $includeconfig) {
+            $quickmailsettings->set_source_sql(
                 'SELECT * FROM {block_quickmail_config}
                 WHERE coursesid = ?', array(array('sqlparam' => $this->get_courseid()))
             );
         }
 
-        $log->annotate_ids('user', 'userid');
-        //$quickmail_settings->annotate_ids('setting');
+        $log->annotate_ids('user', 'user_id');
 
-        $log->annotate_files('block_quickmail', 'log', 'id', $context->id);
-        $log->annotate_files('block_quickmail', 'attachment_log', 'id', $context->id);
-        // $quickmail_settings->annotate_files('block_quickmail', 'settings', 'courseid', $context->id);
-
-        return $this->prepare_block_structure($backup_logs_and_settings);
+        return $this->prepare_block_structure($backuplogsandsettings);
     }
 }
