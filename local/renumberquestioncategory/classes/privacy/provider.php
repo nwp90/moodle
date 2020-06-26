eb51d2d4bd7b8ba993f06845bc658fd420bf0d98
Kimberley Johnston <kimberley.johnston@otago.ac.nz>
2020-06-26 15:56:24 +1200

    Update local/renumberquestioncategory

--- local/renumberquestioncategory/classes/privacy/provider.php
+++ local/renumberquestioncategory/classes/privacy/provider.php
@@ -15,17 +15,35 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Tool for hierarchical numbering of question categories.
+ * Tool for deleting question category with question and subcategories.
  *
  * @package    local_renumberquestioncategory
- * @copyright  2016 Vadim Dvorovenko <Vadimon@mail.ru>
+ * @copyright  2020 Vadim Dvorovenko <Vadimon@mail.ru>
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+namespace local_renumberquestioncategory\privacy;
+
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->component = 'local_renumberquestioncategory';
-$plugin->version   = 2020061300;
-$plugin->release = '1.1';
-$plugin->maturity = MATURITY_STABLE;
-$plugin->requires  = 2015111600; // Moodle 3.0.
+/**
+ * Tool for deleting question category with question and subcategories.
+ *
+ * @package    local_renumberquestioncategory
+ * @copyright  2020 Vadim Dvorovenko <Vadimon@mail.ru>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class provider implements \core_privacy\local\metadata\null_provider {
+
+    use \core_privacy\local\legacy_polyfill;
+
+    /**
+     * Get the language string identifier with the component's language
+     * file to explain why this plugin stores no data.
+     *
+     * @return  string
+     */
+    public static function _get_reason() {
+        return 'privacy:metadata';
+    }
+}
